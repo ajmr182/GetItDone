b083d225dc649cafd3e6003176c1868c5e5dab6f
Amilcar Malaver <ajmr182@gmail.com>
2025-06-16 18:56:07 -0500

    feature: GID-0001 Add android context to GID app

--- app/src/main/java/com/ajdev/getitdone/MainActivity.kt
+++ app/src/main/java/com/ajdev/getitdone/MainActivity.kt
@@ -12,13 +12,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ajdev.getitdone.ui.theme.GetItDoneTheme
+import com.ajdev.getitdone.ui.todoscreen.ToDoScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            GetItDoneTheme {}
+            GetItDoneTheme {
+                ToDoScreen()
+            }
         }
     }
 }
